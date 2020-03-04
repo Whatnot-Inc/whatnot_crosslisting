735a4d8a50a296af0149dc8e135873d6a0fd9c8b
Gustavo Borges <gustavoborges@gustavos-mbp.attlocal.net>
2020-03-04 12:20:29 -0800

    20days

--- pull.py
+++ pull.py
@@ -32,7 +32,7 @@ async def main(loop):
             await asyncio.sleep(100)
             print("Rested...")
             print("Looking for expired listings")
-            await listing_mgmt.republish_older_than(days=28)
+            await listing_mgmt.republish_older_than(days=20)
             print("Resting for a bit.. Again")
             await asyncio.sleep(100)
             print("Rested...")
