99c36730587fe60e8458fb2247aa87c5a07e699a
Gustavo Borges <gustavoborges@gustavos-mbp.attlocal.net>
2020-03-04 12:26:18 -0800

    fixing listing data call

--- app/services/services.py
+++ app/services/services.py
@@ -219,7 +219,7 @@ class ListingManager(BaseService):
             cross_listing = CrossListing.from_dict(record)
             print(f"Republishing listing {cross_listing.sku}")
             self.listing_data = await wn_client.get_listing_by_id(int(cross_listing.listing_id))
-            self.product_data = await wn_client.get_product_by_id(int(listing_data['product_id']))
+            self.product_data = await wn_client.get_product_by_id(int(self.listing_data['product_id']))
             event_data = {'price_cents': cross_listing.price_cents}
             if self.listing_data['status'] == 'active':
                 await self.create(event_data)
